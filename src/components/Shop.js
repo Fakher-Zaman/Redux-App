24fe7ffd047ab86b9631f70be8c6cf0f1d13becb
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-06-18 12:55:14 +0500

    Setting up a React Application for Redux

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div className='container my-3'>
+            <h2>Buy Adidas Shoes 50 Rs</h2>
+            <button className='btn btn-primary m-2'>-</button>
+            Add to cart
+            <button className='btn btn-primary m-2'>+</button>
+        </div>
+    )
+}
+
+export default Shop
