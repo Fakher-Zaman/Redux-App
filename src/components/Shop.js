0bad9a24c716851ea42bef1007ce6d80c66fe443
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-06-19 09:08:08 +0500

    Updating state from a different component in Redux

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux';
+import { actionCreators } from '../state/index';
 
 const Shop = () => {
+    const dispatch = useDispatch();
+    // const action = bindActionCreators(actionCreators, dispatch);
+    const { WithdrawMoney, depositMoney } = bindActionCreators(actionCreators, dispatch);
+
     return (
         <div className='container m-3 d-flex flex-column justify-content-center align-items-center'>
             <h2>Deposit/Withdraw Money</h2>
-            <div className=''>
-                <button className='btn btn-primary m-2'>-</button>
+            {/* <div className='mt-3'>
+                <button className='btn btn-primary m-2' onClick={() => dispatch(actionCreators.WithdrawMoney(100))}>-</button>
                 Update Balance
-                <button className='btn btn-primary m-2'>+</button>
+                <button className='btn btn-primary m-2' onClick={() => dispatch(actionCreators.depositMoney(100))}>+</button>
+            </div> */}
+            {/* <div className='mt-3'>
+                <button className='btn btn-primary m-2' onClick={() => action.WithdrawMoney(100)}>-</button>
+                Update Balance
+                <button className='btn btn-primary m-2' onClick={() => action.depositMoney(100)}>+</button>
+            </div> */}
+            <div className='mt-3'>
+                <button className='btn btn-primary m-2' onClick={() => WithdrawMoney(100)}>-</button>
+                Update Balance
+                <button className='btn btn-primary m-2' onClick={() => depositMoney(100)}>+</button>
             </div>
         </div>
     )
