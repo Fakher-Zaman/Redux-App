005c03e6b21043d5fe3b3d05de86d2a3ddc148b4
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-06-19 09:21:22 +0500

    Finalize Redux

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../state/index';
 
 const Shop = () => {
     const dispatch = useDispatch();
+    const balance = useSelector(state => state.amount);
     // const action = bindActionCreators(actionCreators, dispatch);
     const { WithdrawMoney, depositMoney } = bindActionCreators(actionCreators, dispatch);
 
@@ -23,7 +24,7 @@ const Shop = () => {
             </div> */}
             <div className='mt-3'>
                 <button className='btn btn-primary m-2' onClick={() => WithdrawMoney(100)}>-</button>
-                Update Balance
+                Update Balance ({balance})
                 <button className='btn btn-primary m-2' onClick={() => depositMoney(100)}>+</button>
             </div>
         </div>
